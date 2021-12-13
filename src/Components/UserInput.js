a9049dd650f01dc0921a570bad2e0e38e954e146
NickDempseyDev <nickdempseydev@gmail.com>
2021-12-13 15:18:37 +0000

    created basic UserInput component

---
+++ src/Components/UserInput.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+import './UserInput.css'
+
+const UserInput = ({ sendInfoToParent }) => {
+
+	const [username, setUsername] = useState("")
+	const [option, setOption] = useState("")
+
+	return (
+		<div>
+			<div style={{ display: "flex" , flexFlow: "row", height: "50px", justifyContent: "center"}}>
+				<input type="text" placeholder='Username' onChange={(e) => {setUsername(e.target.value)}}/>
+				<select name="Choose Parameter" id="" onSelect={(e) => {setOption(e.target.value)}}>
+					<option value="repos">Get Repos</option>
+				</select>
+				<button onClick={() => sendInfoToParent({ username, option })}>Run Query</button>
+			</div>
+		</div>
+	)
+}
+
+export default UserInput
